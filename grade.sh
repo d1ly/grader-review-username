b4220a9e24f3eae09805a51ee2639e34c2b020e4
d1ly <d1ly@ucsd.edu>
2023-11-06 11:41:38 -0800

    Autograder

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +14,26 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if [ ! -f "student-submission/ListExamples.java" ]
+then
+    echo "Required file not in respository, need ListExamples.java"
+    exit 1
+fi
+
+cp student-submission/ListExamples.java grading-area/
+cp TestListExamples.java grading-area/
+
+javac -cp "$CPATH" grading-area/*.java 2> errors.txt
+
+if [ $? -ne 0 ]
+then
+    echo "Compilation failed. Check for syntax errors below:"
+    echo ""
+    cat errors.txt
+    exit 1
+fi
+
+java -cp "$CPATH;grading-area" org.junit.runner.JUnitCore TestListExamples > testOutput.txt
+cat testOutput.txt
+#if [grep "FAILURES!!!" testOutput.txt ]
