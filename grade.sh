6aa7e59497e07774e3f3d1e457e030feb9a7b189
d1ly <d1ly@ucsd.edu>
2023-11-27 11:03:12 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -18,6 +18,8 @@ echo 'Finished cloning'
 if [ ! -f "student-submission/ListExamples.java" ]
 then
     echo "Required file not in respository, need ListExamples.java"
+    echo ""
+    echo "Grade: 0/3"
     exit 1
 fi
 
@@ -28,12 +30,25 @@ javac -cp "$CPATH" grading-area/*.java 2> errors.txt
 
 if [ $? -ne 0 ]
 then
-    echo "Compilation failed. Check for syntax errors below:"
+    echo "Compilation failed. Check for syntax or fomatting errors below:"
     echo ""
     cat errors.txt
+    echo ""
+    echo "Grade: 0/3"
     exit 1
 fi
 
 java -cp "$CPATH;grading-area" org.junit.runner.JUnitCore TestListExamples > testOutput.txt
 cat testOutput.txt
-#if [grep "FAILURES!!!" testOutput.txt ]
+CORRECT=$(grep -o "OK" testOutput.txt)
+if [ $CORRECT == "OK" ]
+then
+    echo "Grade: 3/3"
+    exit 1
+fi
+
+TOTAL=$(grep -o "run: "[1-9] testOutput.txt | awk '{ print $2 }')
+FAILURES=$(grep -o "Failures: "[1-9] testOutput.txt | awk '{ print $2 }')
+SCORE=$(($TOTAL - $FAILURES))
+echo ""
+echo "Grade: $SCORE/$TOTAL"
